82837f4ab79c7745fa3ad0b216fa22bcceab4490
shubham24bcy10391-ux <shubham.24bcy10391@vitbhopal.ac.in>
2026-03-28 22:42:29 +0530

    Update script1_system_identity.sh

--- script1_system_identity.sh
+++ script1_system_identity.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Your Name
 
+STUDENT_NAME="Shubham sharma"
+SOFTWARE_CHOICE="Apache HTTP Server"
+
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DISTRO=$(grep PRETTY_NAME /etc/os-release | cut -d= -f2 | tr -d '"')
+DATE=$(date)
+
+echo "================================"
+echo "Open Source Audit — shubham sharma"
+echo "================================"
+echo "Chosen Software: $SOFTWARE_CHOICE"
+echo "Kernel Version: $KERNEL"
+echo "Linux Distribution: $DISTRO"
+echo "Current User: $USER_NAME"
+echo "System Uptime: $UPTIME"
+echo "Current Date: $DATE"
+echo ""
+
+echo "Linux is licensed under the GNU General Public License (GPL)."
