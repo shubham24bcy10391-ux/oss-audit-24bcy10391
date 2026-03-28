ee706c58ccba358ba03511199f9555c61cb5315c
shubham24bcy10391-ux <shubham.24bcy10391@vitbhopal.ac.in>
2026-03-28 22:43:00 +0530

    Update script2_package_inspector.sh

--- script2_package_inspector.sh
+++ script2_package_inspector.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+# Script 2: FOSS Package Inspector
 
+PACKAGE="httpd"
+
+if rpm -q $PACKAGE &>/dev/null; then
+    echo "$PACKAGE is installed."
+    rpm -qi $PACKAGE | grep -E 'Version|License|Summary'
+else
+    echo "$PACKAGE is NOT installed."
+fi
+
+case $PACKAGE in
+    httpd) echo "Apache: the web server that powers the open internet." ;;
+    mysql) echo "MySQL: open-source database used by millions of applications." ;;
+    firefox) echo "Firefox: open-source browser promoting web freedom." ;;
+    vlc) echo "VLC: multimedia player that supports many formats." ;;
+    *) echo "Unknown package." ;;
+esac
